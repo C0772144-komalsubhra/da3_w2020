b7cb6044a2e8001a4650b5de53c2a45cff158157
macstudent <komalsubhra6768@gmail.com>
2020-01-23 17:52:22 -0500

    Commit 3

--- src/main/java/com/lambton/ThirdExample.java
+++ src/main/java/com/lambton/ThirdExample.java
@@ -1,9 +1,19 @@
 package com.lambton;
 
-import java.util.Arrays;
 
 public class ThirdExample {
     public static void main(String[] args) {
-        System.out.println("Welcome hello");
+        int a[];
+        int []b;
+        int c[]=new int[5];
+        c[0] =10;
+        c[1] = 20;
+        c[2] =30;
+        c[3] =40;
+        c[4] =50;
+        a = new int[2];
+        a[0] =100;
+        a[1]=200;
     }
+
 }
