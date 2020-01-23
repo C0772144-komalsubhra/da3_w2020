79de5c463dcac07d8e7863d7073a15b2374e45f8
macstudent <komalsubhra6768@gmail.com>
2020-01-23 17:14:47 -0500

    Commit 2

--- src/main/java/com/lambton/ThirdExample.java
+++ src/main/java/com/lambton/ThirdExample.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 
 public class ThirdExample {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        System.out.println("Welcome hello");
     }
 }
